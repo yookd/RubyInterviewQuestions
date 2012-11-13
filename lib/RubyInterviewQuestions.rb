fbde1617145473ea36b13ce2c9c284957f1ca7f6
David Yoo <dkyoo10@gmail.com>
2012-11-13 09:54:06 -0800

    Add new directory files

--- lib/RubyInterviewQuestions.rb
+++ lib/RubyInterviewQuestions.rb
@@ -1,5 +1,6 @@
 require 'RubyInterviewQuestions/queue'
 require 'RubyInterviewQuestions/stack'
 require 'RubyInterviewQuestions/singly_linked_list'
+require 'RubyInterviewQuestions/singly_linked_list/element'
 module RubyInterviewQuestions
 end
